83bda6876bcc4fc66413b1e8effd82c032946e46
Joao Ferreira <joao.ferreira@riopele.pt>
2022-01-14 12:24:01 +0000

    post example and express-validator examples

--- routes/riopele40_servidores_opcua.js
+++ routes/riopele40_servidores_opcua.js
@@ -3,6 +3,7 @@ var express = require('express')
 var router = express.Router()
 var controller = require('../controllers/riopele40_servidores_opcua')
 const middlewares = require('../utilities/middlewares')
+const { validationResult, body } = require('express-validator')
 
 var validation = function(req, res, next) {
     middlewares.verifyIP(req, res, next)
@@ -15,5 +16,17 @@ router.get('/', [validation], function (req, res) {
 router.get('/raw', [validation], function (req, res) {
     controller.listRaw(req, res);
 })
+
+// POST EXAMPLE
+/*router.post('/', [
+    body('field').notEmpty().isBoolean().isEmail().isNumeric().isString().isArray().optional() // Validations Examples
+], [validation], function (req, res) {
+    const errors = validationResult(req); 
+    if (errors.isEmpty()) {
+        // Continue
+    } else {
+        res.status(400).json({errors: errors.array()})
+    }
+})*/
 
 module.exports = router
