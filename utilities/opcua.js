74d9feb5bf89ba75fd2565a7331061a5ec9a4669
Joao Ferreira <joao.ferreira@riopele.pt>
2022-11-22 11:28:51 +0000

    new features

--- utilities/opcua.js
+++ utilities/opcua.js
@@ -686,6 +686,7 @@ async function recordProduction(identificador_opcua, machine_id, index, order, s
         let setpoint_res = await session_.read(setpoint_obj);
         let setpoint = await setpoint_res.map(result => result.value.value)[0];
 
+        console.log(setpoint_obj, setpoint);
         if(id > 0) {
 
             let num_jogo = null; 
