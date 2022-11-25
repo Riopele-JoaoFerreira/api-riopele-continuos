0f8c799dc0b96806f302d8a11d53d16424648fbe
Joao Ferreira <joao.ferreira@riopele.pt>
2022-11-25 10:11:02 +0000

    new features

--- utilities/connection.js
+++ utilities/connection.js
@@ -4,7 +4,6 @@ const config = require('../config/config').config
 exports.connection = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect,
-    logging: false,
     dialectOptions: {
         multipleStatements: true
     },
