c2c1a6692a21013476b4922baa35a31cd6592930
Joao Ferreira <joao.ferreira@riopele.pt>
2022-12-05 15:23:05 +0000

    bug fix

--- utilities/schedules.js
+++ utilities/schedules.js
@@ -2,16 +2,16 @@ var cron = require('node-cron');
 const utilities_opcua = require('../utilities/opcua')
 
 exports.eventsSchedule = () => {
-    cron.schedule('* * * * *', () => {
+    cron.schedule('*/2 * * * *', () => {
       console.log('Exporting Events');
       utilities_opcua.exportEvents((callback)=> {})
     });
 
-    cron.schedule(' */2 * * * *', () => {
+    cron.schedule(' */5 * * * *', () => {
       console.log('Record Productions');
       utilities_opcua.recordProductions((callback)=> {})
     });
-    cron.schedule('*/30 * * * * *', () => {
+    cron.schedule(' * * * * *', () => {
       console.log('Update Running Orders');
       utilities_opcua.updateOrders((callback)=> {})
   });
