007a06ad6ab492b75710359d4895bfe01d5afedf
Joao Ferreira <joao.ferreira@riopele.pt>
2022-12-07 12:12:30 +0000

    prevent error production

--- utilities/schedules.js
+++ utilities/schedules.js
@@ -2,19 +2,19 @@ var cron = require('node-cron');
 const utilities_opcua = require('../utilities/opcua')
 
 exports.eventsSchedule = () => {
-    cron.schedule('15 * * * * *', () => {
+    cron.schedule('*/2 * * * *', () => {
       console.log(new Date().toLocaleString() + " - Exporting Events");
       utilities_opcua.exportEvents(() => {
         console.log(new Date().toLocaleString() + " - End Exporting Events");
       })
     });
-    cron.schedule('30 * * * * *', () => {
+    cron.schedule('*/5 * * * *', () => {
       console.log(new Date().toLocaleString() + " - Record Productions");
       utilities_opcua.recordProductions(()=> {
         console.log(new Date().toLocaleString() + " - End Record Productions");
       })
     });
-    cron.schedule('45 * * * * *', () => {
+    cron.schedule('1-59/2 * * * *', () => {
       console.log(new Date().toLocaleString() + " - Update Orders");
       utilities_opcua.updateOrders(()=> {
         console.log(new Date().toLocaleString() + " - End Update Orders");
