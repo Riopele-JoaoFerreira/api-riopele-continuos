57923b757dc8fea447dd168a021501d38c443d09
Joao Ferreira <joao.ferreira@riopele.pt>
2023-04-19 11:00:22 +0100

    change intended quantity

--- utilities/opcua.js
+++ utilities/opcua.js
@@ -1285,7 +1285,10 @@ function startGame(data, session_, identificador_opcua) {
                                     where: {
                                         [Op.and]: {
                                             ordem: order[0], 
-                                            cod_sap: data.cod_sap
+                                            cod_sap: data.cod_sap,
+                                            quantidade_produzida: {
+                                                [Op.gt]: 0
+                                            }
                                         }
                                     }
                                 }).then((info) => {
