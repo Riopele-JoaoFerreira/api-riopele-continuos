24b141b755dda8b96489cada3dafe4ee299e52fd
Joao Ferreira <joao.ferreira@riopele.pt>
2023-06-27 11:56:34 +0100

    change lock way

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -266,10 +266,9 @@ exports.check_locked_time = () => {
         }
     ).then(parametro => {
         if (parametro[0].valor > 0) {
-            if(Date.now() - parametro[0].valor > 60000) {
+            if(Date.now() - parametro[0].valor > 1200000) {
                 console.log("Locked Limit Time Exceeded (20minutes), Unlocking") 
                 this.unlock()
-                connect.connect();  
             }
         }
     })
