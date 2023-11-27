481105f4677eee3ad8bcc4669c0e79e128951beb
Riopele-JoaoFerreira <joao.ferreira@riopele.pt>
2023-11-27 17:11:58 +0000

    changes

--- controllers/riopele40_ordens.js
+++ controllers/riopele40_ordens.js
@@ -172,7 +172,6 @@ exports.updateTable = (req, res, id_maquina) => {
                                     }
                                 }
                             } 
-                            console.log(obj);
                             array.push(obj)
                             return callback1()
                         })
